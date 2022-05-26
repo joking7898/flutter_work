88ee435574446352f4ce5f2eb18f16358a6820a4
joking7898 <joking7898@naver.com>
2022-05-27 02:11:26 +0900

    퀴즈 화면 구현

--- lib/screen/screen_quiz.dart
+++ lib/screen/screen_quiz.dart
@@ -1,4 +1,7 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'package:word_app/widget/widget_candidate.dart';
 import '../model/model_quiz.dart';
 
 class QuizScreen extends StatefulWidget {
@@ -14,6 +17,7 @@ class _QuizScreenState extends State<QuizScreen> {
   List<bool> _answerState = [false, false, false, false];
   int _currentIndex = 0;
 
+  @override
   Widget build(BuildContext context) {
     Size screenSize = MediaQuery.of(context).size;
     double width = screenSize.width;
@@ -29,9 +33,92 @@ class _QuizScreenState extends State<QuizScreen> {
             ),
             width: width * 0.85,
             height: height * 0.5,
+            child: Swiper(
+              physics: NeverScrollableScrollPhysics(),
+              loop: false,
+              itemCount: widget.quizs.length,
+              itemBuilder: (BuildContext context, int index) {
+                return _buildQuizCard(widget.quizs[index], width, height);
+              },
+            ),
           ),
         ),
       ),
     );
+  }
+
+  Widget _buildQuizCard(Quiz quiz, double width, double height) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(color: Colors.white),
+        color: Colors.white,
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.fromLTRB(0, width * 0.024, 0, width * 0.024),
+            child: Text(
+              'Q' + (_currentIndex + 1).toString() + '.',
+              style: TextStyle(
+                fontSize: width * 0.06,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Container(
+            width: width * 0.8,
+            padding: EdgeInsets.only(top: width * 0.012),
+            child: AutoSizeText(
+              quiz.title,
+              textAlign: TextAlign.center,
+              maxLines: 2,
+              style: TextStyle(
+                fontSize: width * 0.048,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Container(),
+          ),
+          Column(
+            children: _buildCandidates(width, quiz),
+          )
+        ],
+      ),
+    );
+  }
+
+  List<Widget> _buildCandidates(double width, Quiz quiz) {
+    List<Widget> _children = [];
+    for (int i = 0; i < 4; i++) {
+      _children.add(
+        CandWiget(
+            index: i,
+            width: width,
+            text: quiz.candidates[i],
+            answerState: _answerState[i],
+            tap: () {
+              setState(() {
+                for (int j = 0; j < 4; j++) {
+                  if (j == 1) {
+                    _answerState[j] = true;
+                    _answers[_currentIndex] = j;
+                  } else {
+                    _answerState[j] = false;
+                  }
+                }
+              });
+            }),
+      );
+      _children.add(
+        Padding(
+          padding: EdgeInsets.all(width * 0.024),
+        ),
+      );
+    }
+    return _children;
   }
 }
